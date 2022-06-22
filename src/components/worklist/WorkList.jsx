5d136393c79adf86ae204976a8513863d91aeefe
kangdokyoung <ehrud6672@naver.com>
2022-06-22 13:15:06 +0900

    리스트 수정

--- src/components/worklist/WorkList.jsx
+++ src/components/worklist/WorkList.jsx
@@ -8,94 +8,142 @@ const Wnav = styled.nav`
 
 const Wlogo = styled.h2`
     width: 100%;
-    height: 30vh;
+    height: 25vh;
 `;
+
+const SworkList = styled.div`
+    width: 100%;
+    height: 70vh;
+`
 
 const Wtitle = styled.div`
-padding-left: 12px;
-border-bottom: 1px solid grey;
+    padding-left: 12px;
+    border-bottom: 1px solid grey;
 `;
 
-const Wul = styled.ul`
-    list-style : none;
-    mragin-left: 30px;
-    font-size: 20px
+const Sboard = styled.div`
+    display: flex;
 `;
 
-const Wli = styled.li`
+const SboardName = styled.div`
+    display:flex;
+    overflow:auto;
+`
+
+const Saddiv = styled.div`
+    display: ${props => props.boardClicked === 1 ? 'contents' : 'none'};
     width: 100%;
-    :hover{
-        background: lightgrey;
-        cursor: pointer;
-    }
-`;
+`
 
-const WaddBtn = styled.button`
-    width: 100%;
-    :hover{
-        background: lightgrey;
-        cursor: pointer;
-}`
+const Slist = styled.div`
+    display:flex;
+`
 
+const Slistbody = styled.div`
+    display:flex;
+    overflow:auto;
+`
 const WorkList = ()=>{
+    const [boardClicked, setBoardClicked] = useState(0);
+    const [boardName, setBoardName] = useState('');
     const [board, setBoard] = useState([
         {
             bnum: 1,
             bname: '할 일',
-            work:[
-                {
-
-                }
-            ],
+        },
+        {
+            bnum: 2,
+            bname: '진행 중',
+        },
+        {
+            bnum: 3,
+            bname: '완료',
         },
     ])
+    const [list, setList] = useState([]);
+    const [listName, setListName] = useState('');
     const addBoard = () =>{
         setBoard((prev) =>{
             let newBoard = [
                 ...prev,
                 {
                     bnum: prev.length,
-                    bname: '추가된 보드',
-                    work:[{
-
-                    }],
+                    bname: boardName,
                 }
             ]
             return newBoard;
         })
+        setBoardClicked(0);
+        setBoardName('');
     }
-    const addWorkList = (prev) =>{
-        let newList = [
-            ...prev,
-            {
-                num: prev.length,
-                name: '새로운 공간',
-            }
-        ]
-        return newList;
+    const addList= ()=>{
+        setList((prev)=>{
+            let newList = [
+                ...prev,
+                [
+                    listName
+                ],
+            ]
+            return newList;
+        })
+        setListName('');
     }
+    const inputBoard= (e)=>{
+        setBoardName(e.target.value)
+    };
+
+    const inputList = (e)=>{
+        setListName(e.target.value)
+    };
     
     return(
         <Wnav>
             <Wlogo>작업 목록</Wlogo>
-            <div style={{display:'flex'}}>
-                <div style={{width: '200px'}}>
-                    {board.map((data, i) =>{
-                        return (
-                        <div>
-                            <Wtitle key={i}>{data.bname}</Wtitle>
-                            <Wul key={i+1}>
-                                {data.work && data.work.map((data, i) =>{
-                                    return <Wli key={i}>{data.name}</Wli>
-                                })}
-                            <WaddBtn onClick={()=>{addWorkList()}}>추가하기+</WaddBtn>
-                            </Wul>
-                        </div>
-                        )
-                    })}
-                    <button onClick={()=>{addBoard()}}>보드 추가+</button>
-                </div>
-            </div>
+            <SworkList>
+                <Sboard>
+                    <SboardName>
+                        {board.map((data, i) =>{
+                            return (
+                            <div style={{minWidth: '150px', marginRight:'5px'}}>
+                                <Wtitle key={i}>{data.bname}</Wtitle>
+                            </div>
+                            )
+                        })}
+                        <button onClick={()=>{setBoardClicked(1)}}>보드 추가+</button>
+                    </SboardName>
+                    <Saddiv boardClicked ={boardClicked}>
+                        <input type="text" placeholder="제목 입력"
+                            value={boardName}
+                            onChange={(e)=>{
+                            inputBoard(e);
+                            }}
+                        />
+                        <button type="submit" onClick={()=>{addBoard()}}>+</button>
+                    </Saddiv>
+                </Sboard>
+                <Slist>
+                    <Slistbody>
+                        {board.map((data, i)=>{
+                            return(
+                                <div style={{minWidth: '150px', marginRight: '5px'}} key={i}>
+                                    {list.map((data, i)=>{
+                                        return(
+                                            <div>{list[i]}</div>
+                                        )
+                                    })}
+                                    <input type="text" placeholder="내용 추가"
+                                    value={listName}
+                                    onChange={(e)=>{
+                                        inputList(e);
+                                    }}
+                                    />
+                                    <button type="submit" onClick={()=>{addList()}}>+</button>
+                                </div>
+                            )
+                        })}
+                    </Slistbody>
+                </Slist>
+            </SworkList>
         </Wnav>
     )
 }

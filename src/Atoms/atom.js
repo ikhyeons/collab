3a893d8f7a00ad044a8bccc1478c5659a95c1102
kangdokyoung <ehrud6672@naver.com>
2022-07-13 17:32:51 +0900

    프로젝트 선택 아톰 추가

--- src/Atoms/atom.js
+++ src/Atoms/atom.js
@@ -1,6 +1,7 @@
 import {
     atom,
     selector,
+    atomFamily,
   } from 'recoil';
 
 //
@@ -10,6 +11,32 @@ import {
 //위로는 성익현
 //밑에는 강도경
 
+export const projectState = atom({
+  key: 'projectState',
+  default: [
+    {
+      name:'새 프로젝트1',
+      pic:'',
+    }
+  ],
+});
 
+export const boardState = atomFamily({
+  key: 'boardState',
+  default: [
+    {
+        bnum: 1,
+        bname: '할 일',
+    },
+    {
+        bnum: 2,
+        bname: '진행 중',
+    },
+    {
+        bnum: 3,
+        bname: '완료',
+    },
+  ]
+});
 
 //

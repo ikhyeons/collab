36f10123c00c00d5c67d1850a61790763f5f47c9
ikhyeons <skantrkwl789@naver.com>
2022-07-15 13:48:59 +0900

    댓글 작성란 추가 완료. 및 탬플릿 리코일 적용 완료

--- src/components/doctemplate/DocReplyMain.jsx
+++ src/components/doctemplate/DocReplyMain.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import React, {useState, useRef, useCallback} from 'react'
 import styled from 'styled-components'
 import ReplyText from './docReplyType/ReplyText'
 import { useRecoilState } from 'recoil'
@@ -10,36 +10,57 @@ const SListDiv = styled.ul`
 `
 
 const Sform = styled.form`
-  display : flex;
+  width : 100%;
+`
+
+const SsubmitLine = styled.div`
+  width : 100%;
+  position : relative;
 `
 
 const Sinput = styled.textarea`
+  overflow : hidden;
   resize : none;
-  width : 80%;
+  width : 100%;
   padding : 5px;
   font-size : 17px;
   font-family : none;
   margin : 3px;
+  border : none;
+  border-bottom : 2px solid black;
 `
 
-const Sbutton = styled.button`
+const SSbutton = styled.button`
   padding : 5px;
-  margin : 3px;
-  height : 100%;
+  position : absolute;
+  right : 0px;
 `
 
 
 function ReplyList() {
 
   const [replyLists, setReplyList] = useRecoilState(replyList)
+  const textRef = useRef();
+  const [inputValue, setInputValue] = useState('');
+
+
+  const handleResizeHeight = useCallback(() => {
+    textRef.current.style.height = textRef.current.scrollHeight + "px";
+  }, []);
 
   return (
     <SListDiv>
       <Sform action="">
-        <Sinput type="text" />
-        <Sbutton onClick={(e)=>{
-          e.preventDefault();
-        }}>작성</Sbutton>
+        <Sinput value={inputValue} onInput={handleResizeHeight} onChange={(e)=>{setInputValue(e.target.value)}} ref={textRef} type="text" />
+        <SsubmitLine>
+          <button>a</button>
+          <button>b</button>
+          <button>c</button>
+          <button>d</button>
+          <SSbutton onClick={(e)=>{
+            e.preventDefault();
+          }}>작성</SSbutton>
+        </SsubmitLine>
       </Sform>
         {
           replyLists.map((data, i)=>{

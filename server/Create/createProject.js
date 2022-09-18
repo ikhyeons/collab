00f082509dae7ef010bb5512e097ced15972ffbe
ikhyeons <skantrkwl789@naver.com>
2022-09-18 23:39:54 +0900

    123

--- server/Create/createProject.js
+++ server/Create/createProject.js
@@ -8,7 +8,6 @@ exports.createProject = (req, res) => {
             if(error) throw error;
             con.query('SELECT COUNT(*) FROM collaborator where userNum = ?', [req.session.sid], (error, rows1, fields)=> {
                 if(error) throw error;
-<<<<<<< HEAD
                 con.query('SELECT projectNum FROM project ORDER BY projectNum DESC LIMIT 1', (error, rows2, fields)=> {
                     if (error) throw error;
                     con.query('insert into collaborator values(default, ?, ?, default, ?)', [rows2[0].projectNum, req.session.sid, rows1[0]['COUNT(*)'] + 1], (error, rows, fields)=> {
@@ -16,23 +15,16 @@ exports.createProject = (req, res) => {
                         con.query('SELECT COUNT(*) FROM project', (error, rows3, fields)=> {
                             con.query('insert into workSpace values(default, ?, "문서", "li", 1, default)', [rows3[0]['COUNT(*)']], (error, rows, fields)=> {
                                 con.query('insert into workSpace values(default, ?, "작업목록", "board", 1, default)', [rows3[0]['COUNT(*)']], (error, rows, fields)=> {
-                                    res.send({success : 0});
-                                })
-                            })
-                        })
-=======
-                con.query('insert into collaborator values(default, ?, ?, default, 1)', [rows1[0]['COUNT(*)'], req.session.sid], (error, rows, fields)=> {
-                    if(error) throw error;
-                    con.query('insert into workSpace values(default, ?, "문서", "li", 1, default)', [rows1[0]['COUNT(*)']], (error, rows, fields)=> {
-                        con.query('insert into workSpace values(default, ?, "작업목록", "board", 2, default)', [rows1[0]['COUNT(*)']], (error, rows, fields)=> {
-                            con.query('insert into chatSpace values(default, ?, "전체 채팅", "default", 1, default)', [rows1[0]['COUNT(*)']], (error, rows, fields)=> {
-                                con.query('SELECT COUNT(*) FROM chatSpace', (error, rows2, fields)=> {
-                                    con.query('insert into chatParticipant values(default, ?, 1, default)', [rows2[0]['COUNT(*)'], req.session.sid], (error, rows, fields)=> {
-                                        res.send({success : 0});
+                                    con.query('insert into chatSpace values(default, ?, "전체 채팅", "default", 1, default)', [rows1[0]['COUNT(*)']], (error, rows, fields)=> {
+                                        con.query('SELECT COUNT(*) FROM chatSpace', (error, rows2, fields)=> {
+                                            con.query('insert into chatParticipant values(default, ?, 1, default)', [rows2[0]['COUNT(*)'], req.session.sid], (error, rows, fields)=> {
+                                                res.send({success : 0});
+                                            })
+                                        })
                                     })
                                 })
                             })
->>>>>>> IkhyeonsWork
+                        })
                         })
                 })
             })

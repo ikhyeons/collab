7733561c0d0a8c0986215bc0e3a07cdb5177878b
ikhyeons <skantrkwl789@naver.com>
2022-09-27 19:20:03 +0900

    댓글관련 삭제

---
+++ server/Update/changeReply.js
@@ -0,0 +1,18 @@
+const {mysqlKey}  = require('../mysqlKey');
+const mysql = require('mysql');
+const con = mysql.createConnection(mysqlKey);
+
+exports.changeReply = (req, res) => {
+    const {repNum, innerData} = req.body;
+    if(req.session.logined === true){
+        con.query('UPDATE reply SET innerData = ? WHERE repNum = ?', [innerData, repNum], (error, rows, fields)=> {
+            //응답 변경
+            if(error) throw error;
+            res.send({success : 0});
+        })
+    }
+    else {
+        console.log("로그인 상태가 아닙니다.");
+        res.send({success : 3});
+    }
+    }
